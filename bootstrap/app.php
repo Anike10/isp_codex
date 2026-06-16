fb61316c0be0276f145aec35c9623da04d990d2d
Anike10 <anike10@gmail.com>
2026-06-16 20:16:16 +0600

    Handle expired login sessions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ use App\Http\Middleware\EnsureUserHasPermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +22,21 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (TokenMismatchException $e, Request $request) {
+            return redirect()
+                ->route('login')
+                ->withErrors(['email' => 'Your login session expired. Please sign in again.'])
+                ->withCookie(Cookie::forget(config('session.cookie')));
+        });
+
+        $exceptions->respond(function (Response $response) {
+            if ($response->getStatusCode() !== 419) {
+                return $response;
+            }
+
+            return redirect()
+                ->route('login')
+                ->withErrors(['email' => 'Your login session expired. Please sign in again.'])
+                ->withCookie(Cookie::forget(config('session.cookie')));
+        });
     })->create();
