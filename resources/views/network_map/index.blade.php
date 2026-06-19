b9232f5c0fbfaa75d32781491d020441e1196d0b
Anike10 <anike10@gmail.com>
2026-06-20 02:13:58 +0600

    Refresh deployed network map asset cache key

--- resources/views/network_map/index.blade.php
+++ resources/views/network_map/index.blade.php
@@ -121,5 +121,5 @@
         };
     </script>
     <script src="{{ asset('js/maplibre-gl.js') }}?v=4.7.1"></script>
-    <script src="{{ asset('js/network-map.js') }}?v=20260620-10"></script>
+    <script src="{{ asset('js/network-map.js') }}?v=20260620-11"></script>
 @endsection
